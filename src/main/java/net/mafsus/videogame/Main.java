08dbcf95574fef4a8cf8c91733f69696b80b11ce
veesus mikel heir <noneyourbusiness@nothing.com>
2018-10-29 13:24:05 -0400

    Added basic game window

---
+++ src/main/java/net/mafsus/videogame/Main.java
@@ -0,0 +1,65 @@
+package net.mafsus.videogame;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+
+public class Main {
+
+    static long window;
+    static int windowHeight =600;
+    static int windowWidth=800;
+    public static void gameLoop(){
+
+        GL.createCapabilities();
+
+        GL11.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        while(!GLFW.glfwWindowShouldClose(window)){
+
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+            GLFW.glfwSwapBuffers(window);
+
+
+
+            GLFW.glfwPollEvents();
+
+        }
+    }
+    public static void entry(){
+        initializeGLFW();
+        gameLoop();
+        GLFW.glfwDestroyWindow(window);
+        GLFW.glfwTerminate();
+        GLFW.glfwSetErrorCallback(null).free();
+    }
+    public static void initializeGLFW() {
+        GLFW.glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err));
+        if(!GLFW.glfwInit()){
+            throw new IllegalStateException("Unable to intialize GLFW!");
+        }
+
+        window = GLFW.glfwCreateWindow(windowWidth,windowHeight,"Video Game",NULL,NULL);
+        if(window==NULL){
+            throw new RuntimeException("Failed to create main window!");
+        }
+        GLFW.glfwMakeContextCurrent(window);
+        GLFW.glfwShowWindow(window);
+
+    }
+    public static void main(String[] args){
+        System.out.println("woohoo!");
+        try{
+            entry();
+        }
+        catch(Exception e){
+            System.err.println(e);
+        }
+    }
+}
